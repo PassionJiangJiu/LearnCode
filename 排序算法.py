f2d3f5a1dc4d6a4b9aaac7867694faf750452fe3
officer computer <1025925009@qq.com>
2021-09-14 00:40:06 +0800

    d

--- 排序算法.py
+++ 排序算法.py
@@ -1,3 +1,5 @@
+import math
+
 arr = [5,2,3,15,25,86,34,90,43,65,89,35,78,13,33]
 
 def bubbleSort(arr):
@@ -66,11 +68,47 @@ def shellSort(arr):
 def mergeSort(arr):
     '''归并排序'''
     if(len(arr)<2):
-        print('数组内元素数不能为1')
-        return 
-    
-    
+        return arr
+    mid = int(len(arr)/2)
+    left , right = arr[:mid], arr[mid:]
+    return merge(mergeSort(left),mergeSort(right))
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+        while left:
+            result.append(left.pop(0))
+        while right:
+            result.append(right.pop(0))
+    print(result)
+    return result
+
+def quickSort(arr, pivot, end):
+    '''快速排序'''
+    if pivot >= end:
+        return
+    temp = arr[pivot]
+    low = pivot
+    high = end
+    while low < high:
+        while low < high and arr[high] >= temp:
+            high = high - 1
+        arr[low] = arr[high]
+        while low < high and arr[low] <= temp:
+            low = low + 1
+        arr[high] = arr[low]
+    arr[low] = temp
+    quickSort(arr, pivot, low-1)
+    quickSort(arr, low+1, end)
+    print(arr)
         
+def 
+        
+    
                   
     
 
@@ -78,3 +116,5 @@ def mergeSort(arr):
 # selectionSort(arr)#选择排序
 # insertionSort(arr)#插入排序
 # shellSort(arr)#希尔排序
+# mergeSort(arr)#归并排序
+# quickSort(arr,0,len(arr)-1)#快速排序
